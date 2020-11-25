9bc68c4d158941fe81c58fa60d62379a21c04873
kkkmail <kkk@jfcglobal.net>
2020-11-25 08:25:18 -0500

    20201125

---
+++ MathProblems/MathProblems/MathProblems/EnchantedForest.fs
@@ -0,0 +1,59 @@
+﻿namespace MathProblems
+
+module EnchantedForest =
+
+    type Animal =
+        | Vampire
+        | Spider
+        | Unicorn
+
+
+    type EatingRule =
+        | VampireEatsSpider
+        | VampireEatsUnicorn
+        | SpiderEatsUnicorn
+
+        static member getRule g =
+            match g() % 3 with
+            | 0 -> VampireEatsSpider
+            | 1 -> VampireEatsUnicorn
+            | _ -> SpiderEatsUnicorn
+
+
+    type Forest =
+        {
+            vampires : int
+            spiders : int
+            unicorns : int
+        }
+
+        member f.canEvolve =
+            match f.vampires, f.spiders, f.unicorns with
+            | 0, 0, _ -> false
+            | 0, _, 0 -> false
+            | _, 0, 0 -> false
+            | _ -> true
+
+
+    let tryEat g f =
+        let r = EatingRule.getRule g
+
+        match r with
+        | VampireEatsSpider when f.vampires > 0 && f.spiders > 0 ->
+            { f with vampires = f.vampires - 1; spiders = f.spiders - 1; unicorns = f.unicorns + 1 }
+        | VampireEatsUnicorn when f.vampires > 0 && f.unicorns > 0 ->
+            { f with vampires = f.vampires - 1; spiders = f.spiders + 1; unicorns = f.unicorns - 1 }
+        | SpiderEatsUnicorn when f.spiders > 0 && f.unicorns > 0 ->
+            { f with vampires = f.vampires + 1; spiders = f.spiders - 1; unicorns = f.unicorns - 1 }
+        | _ -> f
+
+
+    let evolve g f =
+        let tryEat = tryEat g
+
+        let rec doEvolve (x : Forest) =
+            match x.canEvolve with
+            | true -> x |> tryEat |> doEvolve
+            | false -> x
+
+        doEvolve f
---
+++ MathProblems/MathProblems/MathProblems/Program.fs
@@ -0,0 +1,35 @@
+﻿open System
+open MathProblems.EnchantedForest
+
+[<EntryPoint>]
+let main argv =
+    printfn "Starting..."
+
+    let f =
+        {
+            vampires = 6
+            spiders = 55
+            unicorns = 17
+        }
+
+//    let f =
+//        {
+//            vampires = 4
+//            spiders = 4
+//            unicorns = 4
+//        }
+
+    let r = Random()
+    let g() = r.Next(1_000_000)
+    let n = 1_000_000
+
+    let f1 =
+        [ for i in 1..n -> i ]
+        |> List.map (fun _ -> evolve g f)
+        |> List.groupBy id
+        |> List.map (fun (a, b) -> (a, b.Length))
+
+    printfn "f = %A\n============================\n" f
+    f1 |> List.map (printfn "%A\n") |> ignore
+    Console.ReadLine() |> ignore
+    0

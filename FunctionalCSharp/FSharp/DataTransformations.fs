f92ee1b5bab670f8ceb82da58124a11a1d7acc49
kkkmail <kkk@jfcglobal.net>
2021-10-24 06:55:37 -0400

    20211024

--- FunctionalCSharp/FSharp/DataTransformations.fs
+++ FunctionalCSharp/FSharp/DataTransformations.fs
@@ -1,4 +1,6 @@
 ﻿namespace FSharp.Lessons
+open System
+open FSharp.Data.Sql
 
 module DataTransformations =
 
@@ -10,29 +12,63 @@ module DataTransformations =
     let toStringError s = s |> StringError |> Error
 
 
+    type ResultData<'T> = Result<'T, ErrorData>
+    type UnitResult = ResultData<unit>
+    type ListResult<'T> = ResultData<List<'T>>
+
+
+    type LoggerProxy =
+        {
+            logError : ErrorData -> unit
+        }
+
+
+    type EmployeeId =
+        | EmployeeId of bigint
+
+        member e.value = let (EmployeeId v) = e in v
+
+
+    type EmployeeName =
+        | EmployeeName of string
+
+        member e.value = let (EmployeeName v) = e in v
+
+
     type Employee =
         {
-            name : string
-            salary : double
+            employeeId : EmployeeId
+            employeeName : EmployeeName
+            dateHired : DateTime
+            salary : decimal
+            description : string option
+        }
+
+
+    type EmployeeProxy =
+        {
+            loadEmployee : EmployeeId -> ResultData<Employee>
+            saveEmployee : Employee -> UnitResult
+            loadEmployees : (Employee -> bool) -> ListResult<Employee>
         }
 
 
     type IncomeRaise =
-        | RaiseByPct of double
-        | RaiseByAmount of double
+        | RaiseByPct of decimal
+        | RaiseByAmount of decimal
 
         static member tryCreateByPctRaise p =
-            if p < 0.0 || p > 1.0 then toStringError $"The percentage parameter: '{p}' is invalid."
+            if p < 0.0m || p > 1.0m then toStringError $"The percentage parameter: '{p}' is invalid."
             else Ok (RaiseByPct p)
 
         static member tryCreateByAmountRaise p =
-            if p < 0.0 || p > 10_000.0 then toStringError $"The amount parameter: '{p}' is invalid."
+            if p < 0.0m || p > 10_000.0m then toStringError $"The amount parameter: '{p}' is invalid."
             else Ok (RaiseByAmount p)
 
 
     let raiseIncome r e =
         match r with
-        | RaiseByPct p -> { e with salary = e.salary * (1.0 + p) }
+        | RaiseByPct p -> { e with salary = e.salary * (1.0m + p) }
         | RaiseByAmount a -> { e with salary = e.salary + a }
 
 

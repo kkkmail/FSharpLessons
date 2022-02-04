310732fa63dbfd96be1125cef9bd3a45371e525e
kkkmail <kkk@jfcglobal.net>
2022-02-04 05:19:53 -0500

    20220204

--- DungeonGenerator/DungeonGenerator/Primitives.fs
+++ DungeonGenerator/DungeonGenerator/Primitives.fs
@@ -2,16 +2,19 @@
 
 module Primitives =
 
-    /// Coordinates of a vertex (dot) on a grid.
     type Coordinates =
         {
             x : int
             y : int
         }
 
+        static member defUltValue =
+            {
+                x = 0
+                y = 0
+            }
 
-    // A square room has only two doors belonging to it: upper and left.
-    /// Bottom and right doors belong to other rooms.
+
     type DoorType =
         | Upper
         | Left
@@ -27,28 +30,39 @@ module Primitives =
 
         static member all = [ Upper; Left ; Bottom ; Right ] |> Set.ofList
 
+        member dt.nextRoom c =
+            match dt with
+            | Upper -> { c with y = c.y - 1 }
+            | Left -> { c with x = c.x - 1 }
+            | Bottom -> { c with y = c.y + 1 }
+            | Right -> { c with x = c.x + 1 }
 
-    /// Coordinates are defined by two doors (upper and left) meeting in the vertex.
-    type Door =
+        member dt.reverse =
+            match dt with
+            | Upper -> Bottom
+            | Left -> Right
+            | Bottom -> Upper
+            | Right -> Left
+
+
+    type DoorToOpen =
         {
             coordinates : Coordinates
             doorType : DoorType
         }
 
-        member d.traverse = 0
-
 
     type Room =
         {
-            coordinates : Coordinates
+            roomNumber : int
             closedDoors : Set<DoorType>
             openedDoors : Set<DoorType>
             invalidDoors : Set<DoorType> // Doors that cannot be used - they are closed and cannot be opened.
         }
 
-        static member create c =
+        static member defaultValue n =
             {
-                coordinates = c
+                roomNumber = n
                 closedDoors = DoorType.all
                 openedDoors = Set.empty
                 invalidDoors = Set.empty
@@ -59,23 +73,89 @@ module Primitives =
             | true -> { r with openedDoors = r.openedDoors |> Set.add dt; closedDoors = r.closedDoors |> Set.remove dt } |> Some
             | false -> None
 
+        member r.tryInvalidate dt =
+            match r.closedDoors |> Set.contains dt with
+            | true -> { r with invalidDoors = r.invalidDoors |> Set.add dt; closedDoors = r.closedDoors |> Set.remove dt } |> Some
+            | false -> None
+
         member r.closedCount = r.closedDoors.Count
         member r.openedCount = r.openedDoors.Count
 
 
-    type Dungeon =
+    type RoomOnMap =
         {
-            rooms : List<Room>
+            coordinates : Coordinates
+            room : Room
         }
 
-        member d.tryAddRoom nextInt =
-            let allOpened =
+
+    let tryFindDoorToOpen (rooms : Map<Coordinates, Room>) (doorNumber : int) : DoorToOpen option =
+        let rec inner (remaining : List<RoomOnMap>) d =
+            match remaining with
+            | [] -> None
+            | h :: t ->
+                if d >= h.room.closedCount
+                then inner t (d - h.room.closedCount)
+                else
+//                    printfn $"d = {d}, closed doors = {h.room.closedDoors}."
+
+                    {
+                        coordinates = h.coordinates
+                        doorType = (h.room.closedDoors |> Set.toList).[d]
+                    }
+                    |> Some
+
+//        printfn $"doorNumber = {doorNumber}."
+        let a = rooms |> Seq.toList |> List.map (fun e -> { coordinates = e.Key; room = e.Value })
+        inner a doorNumber
+
+
+    type Dungeon =
+        {
+            rooms : Map<Coordinates, Room>
+            nextInt : int -> int
+        }
+
+        member d.tryAddRoom () =
+            let allClosed =
                 d.rooms
-                |> List.map (fun e -> e.closedCount)
-                |> List.sum
+                |> Seq.map (fun e -> e.Value.closedCount)
+                |> Seq.sum
 
-            let tryOpen () =
-                let doorNumberToOpen = nextInt (allOpened + 1)
+            let doorNumberToOpen = d.nextInt (allClosed + 1)
 
-                0
-            0
+            match tryFindDoorToOpen d.rooms doorNumberToOpen with
+            | Some v ->
+                let nextRoomCoordinates = v.doorType.nextRoom v.coordinates
+
+                match d.rooms |> Map.tryFind v.coordinates with
+                | Some r0 ->
+                    match d.rooms |> Map.tryFind nextRoomCoordinates with
+                    | Some r ->
+                        match r0.tryInvalidate v.doorType, r.tryInvalidate v.doorType.reverse with
+                        | Some x0, Some x1 -> { d with rooms = d.rooms |> Map.add v.coordinates x0 |> Map.add nextRoomCoordinates x1 }
+                        | _ ->
+                            printfn $"Unable to invalidate door type: {v.doorType} in the room with coordinates {nextRoomCoordinates}. Room: {r}."
+                            d
+                    | None ->
+                        let r = Room.defaultValue d.rooms.Count
+
+                        match r0.tryOpen v.doorType, r.tryOpen v.doorType.reverse with
+                        | Some x0, Some x1 -> { d with rooms = d.rooms |> Map.add v.coordinates x0 |> Map.add nextRoomCoordinates x1 }
+                        | _ ->
+                            printfn $"Something is wrong. Coordinates: {v.coordinates}, room: {r0}, door type: {v.doorType}."
+                            d
+                | None ->
+                    printfn $"Unable to find room with coordinates {v.coordinates} in the dungeon."
+                    d
+            | None ->
+                printfn "Unable to find door to open."
+                d
+
+        static member create nextInt dt =
+            let r = Room.defaultValue 0
+
+            {
+                rooms = [ (Coordinates.defUltValue, { r with openedDoors = r.openedDoors |> Set.add dt; closedDoors = r.closedDoors |> Set.remove dt }) ] |> Map.ofList
+                nextInt = nextInt
+            }
--- DungeonGenerator/DungeonGenerator/Program.fs
+++ DungeonGenerator/DungeonGenerator/Program.fs
@@ -1,2 +1,35 @@
-﻿// For more information see https://aka.ms/fsharp-console-apps
+﻿open System
+open DungeonGenerator
+open DungeonGenerator.Primitives
+
 printfn "Hello from F#"
+
+let rnd = Random(1)
+
+let dungeon = Dungeon.create rnd.Next DoorType.Upper
+
+let steps = [ for i in 1..10 -> i ]
+
+let makeStep i (d : Dungeon) =
+    printfn "\n\n========================================================"
+    printfn $"Making step # {i}."
+    let d1 = d.tryAddRoom()
+
+    d1.rooms
+    |> Seq.toList
+    |> List.map (fun e -> { coordinates = e.Key; room = e.Value })
+    |> List.sortBy (fun e -> e.room.roomNumber)
+    |> List.map (fun e -> printf $"{e}\n\n")
+    |> ignore
+
+    d1
+
+
+printfn "Starting..."
+steps
+|> List.fold (fun acc r -> makeStep r acc) dungeon
+|> ignore
+
+printfn "End."
+Console.ReadLine() |> ignore
+

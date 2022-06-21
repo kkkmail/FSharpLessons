085eeb2215730eb6f3c7062c0fc768545a79192b
kkkmail <kkk@jfcglobal.net>
2022-06-21 18:22:19 -0400

    20220621

---
+++ DungeonGenerator/Coinzz/Data.fs
@@ -0,0 +1,11 @@
+﻿namespace Coinzz
+
+module Data =
+
+    let x = 1
+
+
+    /// Chooses n left and n right coins out of a list of coins
+    let choose a b n =
+
+        0
---
+++ DungeonGenerator/Coinzz/Program.fs
@@ -0,0 +1,4 @@
+﻿open Coinzz.Data
+
+// For more information see https://aka.ms/fsharp-console-apps
+printfn "Hello from F#"

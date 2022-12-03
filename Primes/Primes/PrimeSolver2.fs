60d2272446901ab70234e37182a788c6cafa13ba
kkkmail <kkk@jfcglobal.net>
2022-12-03 17:56:08 -0500

    20221203

---
+++ Primes/Primes/PrimeSolver2.fs
@@ -0,0 +1,64 @@
+﻿namespace Primes
+
+open Open.Numeric.Primes
+open System.Linq
+open System.Diagnostics
+open FSharp.Collections.ParallelSeq
+
+module PrimeSolver2 =
+
+    type SolverParam =
+        {
+            numberOfPrimes : int
+            numberCount : int
+            gcd : int
+            takeNumber: int
+            numberOfSteps : int
+            numberOfFaces : int
+        }
+
+
+    let getBest p primes numbers =
+        printfn "Calculating best numbers with primes..."
+
+        let r =
+            numbers
+            |> PSeq.map (fun ae -> (ae, primes |> List.map (fun pe -> pe + ae) |> List.filter (fun (e : uint64) -> Number.IsPrime(e)) |> Set.ofList))
+            |> PSeq.sortBy (fun (_, b) -> - b.Count)
+            |> Seq.take p.takeNumber
+
+        printfn "Calculating best primes..."
+
+        let q =
+            primes
+            |> PSeq.map (fun pe -> (pe, numbers |> List.map (fun ae -> pe + ae) |> List.filter (fun (e : uint64) -> Number.IsPrime(e)) |> Set.ofList))
+            |> PSeq.sortBy (fun (_, b) -> - b.Count)
+            |> Seq.take p.takeNumber
+            |> Seq.map fst
+            |> Set.ofSeq
+
+        printfn "Constraining primes to the best primes..."
+
+        let w =
+            r
+            |> PSeq.map (fun (ae, pe) -> ae, (pe, q) ||> Set.intersect)
+            |> PSeq.filter (fun (ae, pe) -> pe.Count >= p.numberOfFaces)
+            |> PSeq.sortBy (fun (_, b) -> - b.Count)
+            |> List.ofSeq
+
+        w
+
+    let printBest b =
+        printfn "Printing best combos ..."
+        b
+        |> List.mapi(fun i (a, p) -> printfn $"""i = {i}, a = {a}, P = {p |> Set.toList |> List.sort |> List.map (fun e -> ($"{e}")) |> String.concat ", "}""" )
+        |> ignore
+
+    let solve p =
+        let sw = Stopwatch.StartNew()
+        let primes = Prime.Numbers.Take(p.numberOfPrimes) |> Seq.toList
+        let numbers = [ for i in 1..p.numberCount -> uint64 (i * p.gcd)]
+        let best = getBest p primes numbers
+
+        printBest best
+        0
--- Primes/Primes/Program.fs
+++ Primes/Primes/Program.fs
@@ -1,17 +1,31 @@
-﻿open Primes.PrimeSolver
+﻿open Primes.PrimeSolver2
 open System
 
 printfn "Starting..."
 
+// let p =
+//     {
+//         numberOfPrimes = 200_000
+//         numberCount = 20_000
+//         gcd = 30
+//         takeNumber = 2_000
+//         numberOfSteps = 25
+//     }
+//
+// let result = solve p
+// Console.ReadLine() |> ignore
+
+
 let p =
     {
-        numberOfPrimes = 200_000
-        numberCount = 20_000
+        numberOfPrimes = 20000
+        numberCount = 20000
         gcd = 30
-        takeNumber = 2_000
+        takeNumber = 2000
         numberOfSteps = 25
+        numberOfFaces = 10
     }
 
+
 let result = solve p
-//printfn $"Result: {result}"
 Console.ReadLine() |> ignore
